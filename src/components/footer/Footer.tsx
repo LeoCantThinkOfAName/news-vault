3a45ae3614f934480a64ee760727f1e7abeddebc
LeoCantThinkOfAName <i7851@hotmail.com>
2019-06-18 21:24:45 +0800

    add nyt logo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import moment from "moment";
 
+// styles
 import style from "./footer.module.scss";
+
+// logo
+import logo from "../../images/poweredby_nytimes_65a.png";
 
 const Footer = () => {
   return (
     <footer className={[style["footer"], "container", "footer"].join(" ")}>
-      &copy; {moment().format("YYYY")} ❮NewsVault❯, Powered By New York Times
-      API. <a href="https://lctoan.com">LCTOAN.</a>
+      &copy; {moment().format("YYYY")} ❮NewsVault❯,{" "}
+      <a href="https://lctoan.com">LCTOAN.</a>, Powered By New York Times API.{" "}
+      <img src={logo} alt="Powered By NYT API" className={style["nyt-logo"]} />{" "}
     </footer>
   );
 };
