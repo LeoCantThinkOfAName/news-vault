b00ad5eeda896b4346d123617bd66f8a071d0fa5
LeoCantThinkOfAName <i7851@hotmail.com>
2019-06-18 21:26:32 +0800

    Updates

---
+++ precache-manifest.e243fbbabb18896fbbead6cab9551e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b8e308079a04de9417597bec434ab9",
+    "url": "/news-vault/index.html"
+  },
+  {
+    "revision": "a29b7d9986cbcbd7ea61",
+    "url": "/news-vault/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "8239075d3771a5909b63",
+    "url": "/news-vault/static/css/main.d8c56e20.chunk.css"
+  },
+  {
+    "revision": "a29b7d9986cbcbd7ea61",
+    "url": "/news-vault/static/js/2.44107b2d.chunk.js"
+  },
+  {
+    "revision": "8239075d3771a5909b63",
+    "url": "/news-vault/static/js/main.5f2c8a70.chunk.js"
+  },
+  {
+    "revision": "1bd09abc05c108ad19d1",
+    "url": "/news-vault/static/js/runtime~main.f15583e7.js"
+  }
+]);
